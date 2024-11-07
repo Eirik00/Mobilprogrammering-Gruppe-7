7c6e0d1fd982f181a295f28f2722c666a49d2691
Eirik00 <eirik.rene@gmail.com>
2024-11-07 13:31:53 +0100

    Update SearchPage.kt

--- app/src/main/java/com/gruppe7/wanderly/pages/SearchPage.kt
+++ app/src/main/java/com/gruppe7/wanderly/pages/SearchPage.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -31,7 +32,7 @@ fun SearchPage(tripsViewModel: TripsViewModel, searchText: String, onBack: () ->
                 title = { Text("Search Results for \"$searchText\"") },
                 navigationIcon = {
                     IconButton(onClick = onBack) {
-                        Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
                     }
                 }
             )
