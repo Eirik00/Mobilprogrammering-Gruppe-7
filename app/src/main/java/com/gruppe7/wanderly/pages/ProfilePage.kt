df2bf24751c712043e6dba53831a5c261b0eda56
Eirik00 <eirik.rene@gmail.com>
2024-11-26 20:40:43 +0100

    Update ProfilePage.kt

--- app/src/main/java/com/gruppe7/wanderly/pages/ProfilePage.kt
+++ app/src/main/java/com/gruppe7/wanderly/pages/ProfilePage.kt
@@ -12,9 +12,11 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Edit
@@ -80,6 +82,7 @@ fun ProfilePage(authViewModel: AuthViewModel, tripsViewModel: TripsViewModel, na
 
     Column(
         modifier = Modifier
+            .verticalScroll(rememberScrollState())
             .fillMaxSize()
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
